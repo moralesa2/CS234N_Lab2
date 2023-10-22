8253eed985b1ee96cff0ac6912082361a3a3c375
Aubrey M <moralesa2@my.lanecc.edu>
2023-10-22 12:36:16 -0700

    ProductDB GetProduct, Add/CreateProduct, DeleteProduct methods and unit testing complete

--- MMABooksDBClasses/ProductDB.cs
+++ MMABooksDBClasses/ProductDB.cs
@@ -2,9 +2,127 @@
 using System.Collections.Generic;
 using System.Text;
 
+using MySql.Data.MySqlClient;
+using System.Data;
+using MMABooksBusinessClasses;
+
 namespace MMABooksDBClasses
 {
     public static class ProductDB
     {
+        public static Product GetProduct(string productCode)
+        {
+            MySqlConnection connection = MMABooksDB.GetConnection();
+            string selectStatement
+                = "SELECT ProductCode, Description, UnitPrice, OnHandQuantity "
+                + "FROM Products "
+                + "WHERE ProductCode = @ProductCode";
+            MySqlCommand selectCommand =
+                new MySqlCommand(selectStatement, connection);
+            selectCommand.Parameters.AddWithValue("@ProductCode", productCode);
+
+            try
+            {
+                connection.Open();
+                MySqlDataReader prodReader = 
+                    selectCommand.ExecuteReader(CommandBehavior.SingleRow);
+                if (prodReader.Read())
+                {
+                    Product product = new Product();
+                    product.ProductCode = prodReader["ProductCode"].ToString();
+                    product.Description = prodReader["Description"].ToString();
+                    product.UnitPrice = (decimal)prodReader["UnitPrice"];
+                    product.OnHandQuantity = (int)prodReader["OnHandQuantity"];
+                    return product;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (MySqlException ex) 
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public static string AddProduct(Product product)
+        {
+            MySqlConnection connection = MMABooksDB.GetConnection();
+            string insertStatement =
+                "INSERT Products " +
+                "(ProductCode, Description, UnitPrice, OnHandQuantity) " +
+                "VALUES (@ProductCode, @Description, @UnitPrice, @OnHandQuantity)";
+            MySqlCommand insertCommand = 
+                new MySqlCommand(insertStatement, connection);
+            insertCommand.Parameters.AddWithValue(
+                "@ProductCode", product.ProductCode);
+            insertCommand.Parameters.AddWithValue(
+                "@Description", product.Description);
+            insertCommand.Parameters.AddWithValue(
+                "@UnitPrice", product.UnitPrice);
+            insertCommand.Parameters.AddWithValue(
+                "@OnHandQuantity", product.OnHandQuantity);
+            try
+            {
+                connection.Open();
+                insertCommand.ExecuteNonQuery();
+                string selectStatement =
+                    "SELECT LAST_INSERT_ID()";
+                MySqlCommand selectCommand =
+                    new MySqlCommand(selectStatement, connection);
+                string productCode = Convert.ToString(selectCommand.ExecuteScalar());
+                return productCode;
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public static bool DeleteProduct(Product product)
+        {
+            MySqlConnection connection = MMABooksDB.GetConnection();
+            string deleteStatement =
+                "DELETE FROM Products " +
+                "WHERE ProductCode = @ProductCode " +
+                "AND Description = @Description " +
+                "AND UnitPrice = @UnitPrice " +
+                "AND OnHandQuantity = @OnHandQuantity ";
+            MySqlCommand deleteCommand = 
+                new MySqlCommand(deleteStatement, connection);
+            deleteCommand.Parameters.AddWithValue(
+                "@ProductCode", product.ProductCode);
+            deleteCommand.Parameters.AddWithValue(
+                "@Description", product.Description);
+            deleteCommand.Parameters.AddWithValue(
+                "@UnitPrice", product.UnitPrice);
+            deleteCommand.Parameters.AddWithValue(
+                "@OnHandQuantity", product.OnHandQuantity);
+            try
+            {
+                connection.Open();
+                int recordsAffected = deleteCommand.ExecuteNonQuery();
+                if (recordsAffected == 1)
+                    return true;
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
     }
 }
--- MMABooksTests/ProductDBTests.cs
+++ MMABooksTests/ProductDBTests.cs
@@ -2,9 +2,54 @@
 using System.Collections.Generic;
 using System.Text;
 
+using NUnit.Framework;
+using MMABooksBusinessClasses;
+using MMABooksDBClasses;
+
 namespace MMABooksTests
 {
     public class ProductDBTests
     {
+        [Test]
+        public void TestGetProduct()
+        {
+            Product p = ProductDB.GetProduct("A4CS");
+            Assert.AreEqual("A4CS", p.ProductCode);
+        }
+
+        [Test]
+        public void TestCreateProduct()
+        {
+            Product p = new Product();
+            p.ProductCode = "T35T";
+            p.Description = "Test description1";
+            p.UnitPrice = 101.01M;
+            p.OnHandQuantity = 1;
+
+            ProductDB.AddProduct(p);
+            Assert.AreEqual("Test description1", p.Description);
+
+            ProductDB.DeleteProduct(p);
+            //deleting test data for cleanup & to avoid duplicate entry exception when test run more than once
+        }
+
+        [Test]
+        public void TestDeleteProduct()
+        {
+            Product p = new Product();
+            p.ProductCode = "TESTCODE";
+            p.Description = "Test description2";
+            p.UnitPrice = 102.01M;
+            p.OnHandQuantity = 3;
+
+            ProductDB.AddProduct(p);
+            Assert.IsTrue(ProductDB.DeleteProduct(p));
+        }
+
+        [Test]
+        public void TestUpdateProduct()
+        {
+
+        }
     }
 }
